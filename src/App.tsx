8af99f84ab1c9200163952a9b2751337d3be470f
Arnav Goel <arnav.goel1@outlook.com>
2023-12-31 15:51:10 +0530

    Added Responsive Layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,33 @@
 import "./App.css";
-import { Button } from "@mui/material";
+import { Button, Hidden, Paper, styled } from "@mui/material";
+import Grid from "@mui/material/Unstable_Grid2";
 
 function App() {
-  return <Button variant="contained">Contained</Button>;
+  const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: "center",
+    color: theme.palette.text.secondary,
+  }));
+
+  return (
+    <>
+      <Grid container spacing={2}>
+        <Grid xs={12}>
+          <Item>Navbar</Item>
+        </Grid>
+        <Hidden mdDown>
+          <Grid xs={3} lg={6}>
+            <Item>Aside</Item>
+          </Grid>
+        </Hidden>
+        <Grid xs>
+          <Item>Content</Item>
+        </Grid>
+      </Grid>
+    </>
+  );
 }
 
 export default App;
