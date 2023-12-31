3a1676afabd10fe1d7c950360a1f9b62afdb435c
Arnav Goel <arnav.goel1@outlook.com>
2023-12-31 16:18:41 +0530

    Fix

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,14 +1,22 @@
 import { Typography } from "@mui/material";
 import logo from "../assets/logo.png";
-import React from "react";
 
 const NavBar = () => {
   return (
-    <div style={{ display: "flex", alignItems: "center" }}>
-      <img src={logo} width={40} alt="Logo" style={{ marginRight: 16 }} />
-      <Typography variant="body1" style={{ fontSize: "1.5rem" }}>
-        NavBar
-      </Typography>
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "space-between",
+      }}
+    >
+      <div style={{ display: "flex", alignItems: "center" }}>
+        <img src={logo} width={40} alt="Logo" style={{ marginRight: 16 }} />
+        <Typography variant="body1" style={{ fontSize: "1.5rem" }}>
+          NavBar
+        </Typography>
+      </div>
+      <Typography variant="body1">check</Typography>
     </div>
   );
 };
