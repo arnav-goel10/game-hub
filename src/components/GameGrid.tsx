e8e34b2175a76232f6a0cfab2e9d2b72ad9ce9df
Arnav Goel <arnav.goel1@outlook.com>
2024-01-01 23:06:30 +0530

    FIXEDuseGamesService

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,29 +1,7 @@
-import { useEffect, useState } from "react";
-import apiClient from "../services/api-client";
-
-interface Game {
-  id: number;
-  name: string;
-}
-
-interface FetchGamesResponse {
-  count: number;
-  results: Game[];
-}
+import useGames from "../hooks/useGames";
 
 const GameGrid = () => {
-  const [games, setGames] = useState<Game[]>([]);
-  const [error, setError] = useState("");
-
-  useEffect(() => {
-    apiClient
-      .get<FetchGamesResponse>("/games")
-      .then((games) => setGames(games.data.results))
-      .catch((err) => {
-        setError(err.message);
-      });
-  });
-
+  const { games, error } = useGames();
   return (
     <>
       {error && <div>{error}</div>}
