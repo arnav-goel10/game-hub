d6a1b674f65efb3fc28b2ee9fdd991035409fe3d
Arnav Goel <arnav.goel1@outlook.com>
2024-01-02 23:38:38 +0530

    formagt

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,34 +1,40 @@
 import { useEffect, useState } from "react";
 import apiClient from "../services/api-client";
 import { CanceledError } from "axios";
-export interface Game {
-    id: number;
-    name: string;
-    background_image: string;
-  }
-  
-  interface FetchGamesResponse {
-    count: number;
-    results: Game[];
-  }
-const useGames = () => {
-    const [games, setGames] = useState<Game[]>([]);
-    const [error, setError] = useState("");
-      
-    useEffect(() => {
-        const controller = new AbortController();
-
-        apiClient
-        .get<FetchGamesResponse>("/games",{signal:controller.signal})
-        .then((games) => setGames(games.data.results))
-        .catch((err) => {
-            if (err instanceof CanceledError)  return ;
-            setError(err.message);
-    });
-        return () => controller.abort();
-    },[]);
-       
-    return {games,error}
-    
+interface Platform {
+  id: string;
+  name: string;
+  slug: string;
 }
-export default useGames
+
+export interface Game {
+  id: number;
+  name: string;
+  background_image: string;
+  parent_platforms: { platform: Platform }[];
+}
+
+interface FetchGamesResponse {
+  count: number;
+  results: Game[];
+}
+const useGames = () => {
+  const [games, setGames] = useState<Game[]>([]);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    const controller = new AbortController();
+
+    apiClient
+      .get<FetchGamesResponse>("/games", { signal: controller.signal })
+      .then((games) => setGames(games.data.results))
+      .catch((err) => {
+        if (err instanceof CanceledError) return;
+        setError(err.message);
+      });
+    return () => controller.abort();
+  }, []);
+
+  return { games, error };
+};
+export default useGames;
--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: 'https://api.rawg.io/api',
-    params: {
-        key: '08f2bfd96c5048259849c772a211a48f'
-    }
-})
+  baseURL: "https://api.rawg.io/api",
+  params: {
+    key: "08f2bfd96c5048259849c772a211a48f",
+  },
+});
