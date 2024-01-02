d62a068c40136a3e6286f3bbbe6dfb66150f4069
Arnav Goel <arnav.goel1@outlook.com>
2024-01-03 00:21:54 +0530

    Added Icons

--- src/components/PlatformIconList.tsx
+++ src/components/PlatformIconList.tsx
@@ -1,15 +1,42 @@
 import { Platform } from "../hooks/useGames";
-import { Typography } from "@mui/material";
+import { Icon, SvgIcon, Typography } from "@mui/material";
+import {
+  FaWindows,
+  FaPlaystation,
+  FaXbox,
+  FaApple,
+  FaLinux,
+  FaAndroid,
+} from "react-icons/fa";
+import { SiNintendo } from "react-icons/si";
+import { MdPhoneIphone } from "react-icons/md";
+import { BsGlobe } from "react-icons/bs";
+import { IconType } from "react-icons";
 
 interface Props {
   platforms: Platform[];
 }
 
 const PlatformIconList = ({ platforms }: Props) => {
+  const platformIcons: { [key: string]: IconType } = {
+    pc: FaWindows,
+    playstation: FaPlaystation,
+    xbox: FaXbox,
+    mac: FaApple,
+    linux: FaLinux,
+    android: FaAndroid,
+    ios: MdPhoneIphone,
+    web: BsGlobe,
+    nintendo: SiNintendo,
+  };
   return (
     <>
       {platforms.map((platform) => (
-        <Typography>{platform.name}</Typography>
+        <Icon
+          key={platform.slug as string}
+          component={platformIcons[platform.slug]}
+          sx={{ marginRight: 1 }}
+        />
       ))}
     </>
   );
